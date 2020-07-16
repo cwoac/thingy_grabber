ff24d747de6dd572ecdff2485b7001db37e7c5c2
Oliver Matthews <oliver@codersoffortune.net>
2020-07-16 11:30:34 +0100

    Fix minor typo, add thingy_compress.sh to allow compressing of existing downloads

---
+++ thingy_compress.sh
@@ -0,0 +1,26 @@
+#!/bin/sh
+sourcefile=${1%/}
+
+if [ ! -d "$sourcefile" ]
+then
+    echo "$1 does not appear to be a directory."
+    exit 1
+fi
+
+timestamp=$(cat "$sourcefile/timestamp.txt")
+
+if [ -z "$timestamp" ]
+then
+    echo "No timestamp?"
+    exit 1
+fi
+
+output="${sourcefile} - ${timestamp}.7z"
+
+if [ -f "${output}" ]
+then
+    echo "Target file ${output} already exists"
+    exit 1
+fi
+
+7z a -mx=9 "${output}" "${sourcefile}"
